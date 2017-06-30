98d6497ea8d0296712736bf3b8bd0cb9c88a3ceb
Richard Ostler <richard.ostler@rothamsted.ac.uk>
2017-06-30 12:25:49 +0100

    Added some comments

--- doipress.py
+++ doipress.py
@@ -7,16 +7,6 @@ Created on 26 Jun 2017
 @organization: Rothamsted Research
 @version: 1.0
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
-to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR 
-OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-DEALINGS IN THE SOFTWARE.
 '''
 import tkinter as tk
 from tkinter import ttk
@@ -43,9 +33,12 @@ def generateDOI(*args):
         data = cursor.fetchone()
         nextId = data[0]
         
+        # Generate the DOI
         now = str(date.today()) + ":" + str(time.time())
         hashids = Hashids(min_length=16, salt=now.__str__())
         doi.set(doiPrefix+"/" + hashids.encode(int(nextId)))
+        
+        # Log the new DOI to the database
         cursor.execute("insert into DOIManager (doi_sequence,doi,salt,created_by) values(" + str(nextId) + ",'"+doi.get()+"','"+now+"','"+getpass.getuser()+"')")
         db.commit()
         db.close()
@@ -74,7 +67,6 @@ class MainApplication(tk.Frame):
         
         img = tk.Image("photo", file="rothamsted-favicon.ico")
         root.tk.call('wm','iconphoto',root._w,img)
-        #root.iconbitmap("rothamsted-favicon.png")
         mainframe = ttk.Frame(root, padding="12 12 12 12")
         mainframe.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
         mainframe.columnconfigure(0, weight=1)
